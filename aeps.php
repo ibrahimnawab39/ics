ecbcf8b4ba771482a1a282d717beb7b24a826b74
zaid2255 <zaidiftikhar63@gmail.com>
2022-10-17 23:04:30 +0500

    checj
    check

--- aeps.php
+++ aeps.php
@@ -36,11 +36,10 @@
 <div class="conatiner-fluid content-inner mt-n5 py-0">
     <div class="row">
         <div class="card" style="width: 18rem;">
-            <ul class="list-group list-group-flush">
-                <li class="list-group-item">An item</li>
-                <li class="list-group-item">A second item</li>
-                <li class="list-group-item">A third item</li>
-            </ul>
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
             <div class="card-footer">
                 Card footer
             </div>
