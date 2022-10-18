c53b28b85e778b7856cbc8eb8f017a3e2ac354ac
avnishtiwari456 <111576647+avnishtiwari456@users.noreply.github.com>
2022-10-18 22:21:19 +0530

    fe
    fe

--- header.php
+++ header.php
@@ -57,7 +57,7 @@
                     <rect x="10.5366" y="16.3945" width="16" height="4" rx="2" transform="rotate(45 10.5366 16.3945)" fill="currentColor"/>
                     <rect x="10.5562" y="-0.556152" width="28" height="4" rx="2" transform="rotate(45 10.5562 -0.556152)" fill="currentColor"/>
                 </svg>
-                <!--logo End-->            <h4 class="logo-title">PlusPay</h4>
+                <!--logo End-->            <h4 class="logo-title">SaralPe</h4>
             </a>
             <div class="sidebar-toggle" data-toggle="sidebar" data-active="true">
                 <i class="icon">
