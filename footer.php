616063654f25469b97ca38ecf1e9a1682f2c5daf
zaid2255 <zaidiftikhar63@gmail.com>
2022-10-19 06:18:09 +0500

    transaction

--- footer.php
+++ footer.php
@@ -57,14 +57,11 @@
     
     <!-- App Script -->
     <script src="assets/js/hope-ui.js" defer></script>
+    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
   </body>
 </html>
-
-
-
-
-
-
-
-
-
+<script>
+    $(document).ready(function() {
+    $('select').select2();
+});
+</script>
--- header.php
+++ header.php
@@ -37,6 +37,10 @@
       
       <!-- RTL Css -->
       <link rel="stylesheet" href="assets/css/rtl.min.css"/>
+
+      <!-- select2 -->
+      <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+      
       
   </head>
   <body class="  ">
